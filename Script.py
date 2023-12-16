9695311d2c7ad796244d1a3fad8c1f8f41a12a6e
Naruto46i <150582863+Naruto46i@users.noreply.github.com>
2023-12-16 12:38:34 +0530

    FIX

--- Script.py
+++ Script.py
@@ -359,27 +359,9 @@ Elsa
     HORRID_TXT = """<b>GROUP MANAGING
     REPORTS : /report or @admin
     Reports
-
 We're all busy people who don't have time to monitor our groups 24/7. But how do you react if someone in your group is spamming?
-
-Presenting reports; if someone in your group thinks someone needs reporting, they now have an easy way to call all admins.
-
-User commands:
 - /report: Reply to a message to report it for admins to review.
 - admin: Same as /report
-
-Admin commands:
-- /reports <yes/no/on/off>: Enable/disable user reports.
-
-To report a user, simply reply to his message with @admin or /report; Rose will then reply with a message stating that admins have been notified. This message tags all the chat admins; same as if they had been @'ed.
-
-Note that the report commands do not work when admins use them; or when used to report an admin. Rose assumes that admins don't need to report, or be reported!
-
- Warnings :
-
-Keep your members in check with warnings; stop them getting out of control!
-
-Admin commands:
 - /warn  |  or /warn  in reply to a message: warn a user
 - /removelastwarn: remove the last warn that a user has received
 - /getwarns: list the warns that a user has received
